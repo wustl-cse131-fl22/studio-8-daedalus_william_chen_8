cee58aaf7ec88c658dedb4a73ce87d71116af6f9
DRACula <c.daedalus@wustl.edu>
2022-11-17 11:50:35 -0600

    Date Implementation

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,9 +1,42 @@
 package studio8;
 
+import java.util.Objects;
+
 public class Date {
+	private int month;
+	private int date;
+	private int year;
 
+    public Date(int month, int date, int year) {
+		super();
+		this.month = month;
+		this.date = date;
+		this.year = year;
+	}
 
-    public static void main(String[] args) {
+	@Override
+	public String toString() {
+		return "Date: " + month + "/" + date + "/" + year;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, month, year);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return date == other.date && month == other.month && year == other.year;
+	}
+
+	public static void main(String[] args) {
 
     }
 
